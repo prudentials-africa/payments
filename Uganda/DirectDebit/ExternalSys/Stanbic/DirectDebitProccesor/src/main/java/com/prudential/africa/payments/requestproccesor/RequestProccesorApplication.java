af1dcd5c1c6db203fcb1f1b926c7c2b336a9f5fe
Ganesh-accolite <35957868+Ganesh-accolite@users.noreply.github.com>
2018-08-02 15:47:35 +0530

    Intial setup for Stanbic DirectDebit Processor
    Intial setup for Stanbic DirectDebit Processor

---
+++ Uganda/DirectDebit/ExternalSys/Stanbic/DirectDebitProccesor/src/main/java/com/prudential/africa/payments/requestproccesor/RequestProccesorApplication.java
@@ -0,0 +1,12 @@
+package com.prudential.africa.payments.requestproccesor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestProccesorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestProccesorApplication.class, args);
+	}
+}
