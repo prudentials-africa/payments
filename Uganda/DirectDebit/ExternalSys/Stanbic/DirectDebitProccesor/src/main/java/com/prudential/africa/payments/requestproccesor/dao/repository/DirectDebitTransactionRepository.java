93016ca634b2c9e2eb3a2914629777bbd9338ca3
Ganesh-accolite <35957868+Ganesh-accolite@users.noreply.github.com>
2018-08-03 15:31:07 +0530

    Updated code for xml generation
    Updated code for xml generation

--- Uganda/DirectDebit/ExternalSys/Stanbic/DirectDebitProccesor/src/main/java/com/prudential/africa/payments/requestproccesor/dao/repository/DirectDebitTransactionRepository.java
+++ Uganda/DirectDebit/ExternalSys/Stanbic/DirectDebitProccesor/src/main/java/com/prudential/africa/payments/requestproccesor/dao/repository/DirectDebitTransactionRepository.java
@@ -3,9 +3,11 @@ package com.prudential.africa.payments.requestproccesor.dao.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.prudential.africa.payments.requestproccesor.dao.entities.DirectDebitTransaction;
 
+@Repository
 public interface DirectDebitTransactionRepository extends CrudRepository<DirectDebitTransaction, Long> {
 
 	List<DirectDebitTransaction> findByBatchNumber(String batchNumber);
